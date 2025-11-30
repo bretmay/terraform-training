cf79cee1b23e26989985cdc937c32da4d73e7dab
bmay <bret.may@gmail.com>
2025-11-30 09:29:42 -0700

    initial commit

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "resource_group_name" {
+  description = "The name of the Azure resource group"
+  type        = string
+  default     = "brets-resources-rg"
+}
+
+variable "location" {
+  description = "The Azure region where resources will be created"
+  type        = string
+  default     = "East US"
+}
+
+variable "environment" {
+  description = "The environment name (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
